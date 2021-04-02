b98b29a11563d19571256264ce4b8620c23d1a05
mohammedgamal91055 <mg91055@gmail.com>
2021-04-02 19:42:26 +0200

    product card details

--- src/app/features/products/components/product-details/product-details.component.ts
+++ src/app/features/products/components/product-details/product-details.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { async } from '@angular/core/testing';
+import { IProduct } from './../../../../shared/models/product';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProductsService } from 'src/app/shared/services/products.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss']
 })
-export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
+  item: IProduct;
+  subsecription: Subscription = new Subscription();
+  constructor(
+    private ac: ActivatedRoute,
+    private productsService: ProductsService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.subsecription.add(
+      this.ac.params.subscribe(params => {
+        this.productsService.getAllProducts().subscribe(products => {
+          this.item = products.find(p => p.id == params.id)
+        })
+      })
+    )
+  }
+  ngOnDestroy() {
+    this.subsecription.unsubscribe()
+  }
 }
