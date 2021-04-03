5c7206e63e8b720778856b8b9a31c0f2fc2e33e3
mohammedgamal91055 <mg91055@gmail.com>
2021-04-03 15:51:29 +0200

    cart side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from './shared/models/product';
+import { CartService } from './shared/services/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task';
+  is_Sidebar_Oppened = false;
+  constructor(
+    public cartService: CartService
+  ) { }
+
+  updateCartItem(op, item: IProduct) {
+    switch (op) {
+      case '-':
+        item.itemCount--;
+        if (item.itemCount > 0) {
+          this.cartService.addItemToCart(item);
+        } else {
+          this.cartService.removeItemFromCart(item);
+        }
+        break;
+      case '+':
+        item.itemCount++;
+        this.cartService.addItemToCart(item);
+        break;
+    }
+  }
 }
